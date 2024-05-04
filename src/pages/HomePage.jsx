1282e0e1635f8a832538898fe7a99979bdaf6dd9
jklama <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>
2024-05-04 10:52:07 +0545

    added upto link tag to navbar 131

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
+import HomeCards from "../components/HomeCards";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default HomePage;
